206b80a1242d7087a30f1a59252c79cdd8c73613
Jane S <sferrazzajane@gmail.com>
2021-01-20 13:58:18 -0500

    working on blank stuff

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 var guessBlank = document.querySelector(".blanklinesmatter");
 var words = ["Saab", "Volvo", "BMW"];
+var splitComputerChoices = [];
+var blankLines = [];
+var blankArray = [];
 
 var computerChoices = words[Math.floor(Math.random() * words.length)];
 splitComputerChoices = computerChoices.split(" ");
-blankLines = splitComputerChoices.length;
-blankArray = [];
-
+blankLines = splitComputerChoices[0].length;
+// blankArray = [];
+var letterindex = 0;
 for (var i = 0; i < blankLines; i++){
     blankArray.push("_");
+    guessBlank.textContent = blankArray.join(" ")
     
 }
-guessBlank.textContent = blankArray.join("")
+console.log(computerChoices);
+console.log(blankLines);
+console.log(splitComputerChoices);
+
 
