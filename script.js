e6b7b11d9020df08eeeddc5271adfeb9914f985b
Ben Gross <benrgross@gmail.com>
2021-01-20 13:31:07 -0500

    update.

---
+++ script.js
@@ -0,0 +1,12 @@
+// Define Elements
+// start button
+const startBtn = document.getElementById("startGame");
+
+// vars for wins and losses
+var wins = 0;
+var loses = 0;
+
+// event listener for start btn
+startBtn.addEventListener("click", startGame());
+
+function startGame() {}
